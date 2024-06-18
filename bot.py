530e9c8cc5a60492e79393fc06efab61ac142b10
Temirlan <temirlanmukatayev@gmail.com>
2024-06-18 13:21:27 +0500

    answer to start command

---
+++ bot.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+from config_reader import config
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher()
+
+# Handler for command /start
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer('Hello world!')
+
+# Start polling for new updates
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
